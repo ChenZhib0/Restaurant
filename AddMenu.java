0bfd467a2b1c7a8ec6874f3c31deeeaa4189a7d8
ChenZhib0 <148733707+ChenZhib0@users.noreply.github.com>
2023-11-04 21:15:13 +0800

    Add files via upload

---
+++ AddMenu.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddMenu {
+    private ArrayList<Menu> menus=new ArrayList<>();
+
+    Scanner input=new Scanner(System.in);
+
+    public void initialMenu(){
+        Menu menu=new Menu();
+        String group="meat";
+        menu.setGroup(group);
+        String name ="111";
+        menu.setCuisine(name);
+        double money=15.00;
+        menu.setMoney(money);
+        double money1=12.00;
+        menu.setMoney1(money1);
+        menus.add(menu);
+
+
+
+
+
+
+    }
+    public void addMenu(){
+        Menu menu=new Menu();
+            System.out.println("choose group(meat/fish/vegetable/snack/drinks)");
+            String group = input.next();
+            menu.setGroup(group);
+
+            System.out.println("input name");
+            String name = input.next();
+            menu.setCuisine(name);
+
+            System.out.println("input money");
+            double money = input.nextDouble();
+            menu.setMoney(money);
+
+            //System.out.println("input money1");
+            double money1 = money*0.8;
+            menu.setMoney1(money1);
+
+            menus.add(menu);
+
+
+            System.out.println("success");
+
+
+    }
+
+
+    public void deleteMenu(){
+
+
+        System.out.println("input name");
+        String name = input.next();
+        int i;
+        for (i = 0; i < menus.size(); i++) {
+            Menu menu = menus.get(i);
+            if (menu.getCuisine().equals(name)) {
+                break;
+            }
+        }
+        Menu menu = menus.get(i);
+        if(menu.getCuisine().equals(name)) {
+            menus.remove(menu);
+            System.out.println("success");
+        }
+        else{
+            System.out.println("This cuisine does not exist");
+        }
+
+
+
+
+    }
+
+    public void showMenu(String group){
+        for (int i = 0; i <menus.size(); i++) {
+            Menu menu=menus.get(i);
+            //if(menu.getGroup().equals(group)){
+              System.out.println(menu.getCuisine());
+              System.out.println(menu.getMoney());
+              System.out.println("-----------");//}
+        }
+    }
+    public void showMenu1(String group){
+        for (int i = 0; i <menus.size(); i++) {
+            Menu menu=menus.get(i);
+            //if(menu.getGroup().equals(group)){
+              System.out.println(menu.getCuisine());
+              System.out.println(menu.getMoney1());
+              System.out.println("-----------");//}
+        }
+    }
+
+    public int getCuisineByName(String name){
+        int money=0;
+
+        for (int i = 0; i < menus.size(); i++) {
+            Menu menu = menus.get(i);
+            if (menu.getCuisine().equals(name)) {
+                money+=menu.getMoney();
+                return money;
+            }
+        }
+        return 0;
+
+    }
+
+    public int getCuisineByName1(String name){
+        int money=0;
+
+        for (int i = 0; i < menus.size(); i++) {
+            Menu menu = menus.get(i);
+            if (menu.getCuisine().equals(name)) {
+                money+=menu.getMoney1();
+                return money;
+            }
+        }
+        return 0;
+
+    }
+
+}
