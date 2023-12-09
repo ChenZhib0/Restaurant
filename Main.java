c991389ea65b9228723d18ba74890f760404b44d
ChenZhib0 <148733707+ChenZhib0@users.noreply.github.com>
2023-12-09 16:17:22 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //创建系统
         Order order=new Order();
         //启动系统
